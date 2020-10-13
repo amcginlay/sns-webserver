80f91650b59f186350727c5106517cdc2fdbc995
Alan McGinlay <amcginlay@users.noreply.github.com>
2020-10-13 17:24:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ server.listen(port, () => { console.log('Server listening at port %d', port); })
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.post('/', (req, res) => {
-//     console.log(req.body)
-    console.log(req)
-    res.end();
+  console.log("<--REQ-->")
+  console.log(req)
+  console.log("<--BODY-->")
+  console.log(req.body)
+  res.end();
 })
