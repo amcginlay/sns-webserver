cc999bfc87965bd0c79d8ae01a6f57d1c4470fe3
Alan McGinlay <amcginlay@users.noreply.github.com>
2020-10-13 18:13:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ var port = process.env.PORT || 3000;
 
 server.listen(port, () => { console.log('Server listening at port %d', port); });
 app.use(express.json({type: ['application/json','text/plain']}));
-app.all('/', (req, res) => {
-  if (req.method === "POST") {
-    console.log("SNS Subscription Token: " + req.body.Token);
-  } else {
-    console.log("SNS Publication Received: " + req.body);
-  }
+app.post('/', (req, res) => {  
+  if (req.body.Token != null)   console.log("SNS Subscription Token: " + req.body.Token);
+  if (req.body.Message != null) console.log("SNS Message: " + req.body.Message);
   res.end();
 })
