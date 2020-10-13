2132bd8441762f7be0b7e49099af2be70d67579e
Alan McGinlay <amcginlay@users.noreply.github.com>
2020-10-13 17:55:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 server.listen(port, () => { console.log('Server listening at port %d', port); });
 //app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.json({type: ['application/json','text/plain'}));
+app.use(express.json({type: ['application/json','text/plain']}));
 app.post('/', (req, res) => {
   console.log("<--REQ-->");
   console.log(req);
